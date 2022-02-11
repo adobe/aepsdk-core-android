6f998a0052101c52feb5d060d9fd59d0fabfefa0
Yansong <yanyang@adobe.com>
2022-02-11 14:46:47 -0700

    no message

--- code/android-core-library/src/androidTest/java/com/adobe/marketing/mobile/AndroidNetworkServiceOverriderTests.java
+++ code/android-core-library/src/androidTest/java/com/adobe/marketing/mobile/AndroidNetworkServiceOverriderTests.java
@@ -9,150 +9,150 @@
   governing permissions and limitations under the License.
  */
  
-package com.adobe.marketing.mobile;
+// package com.adobe.marketing.mobile;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Map;
-import com.adobe.marketing.mobile.AndroidNetworkServiceOverrider.HTTPConnectionPerformer;
-import com.adobe.marketing.mobile.AndroidNetworkServiceOverrider.Connecting;
+// import java.io.BufferedOutputStream;
+// import java.io.IOException;
+// import java.io.InputStream;
+// import java.io.OutputStream;
+// import java.net.HttpURLConnection;
+// import java.net.MalformedURLException;
+// import java.net.URL;
+// import java.util.Map;
+// import com.adobe.marketing.mobile.AndroidNetworkServiceOverrider.HTTPConnectionPerformer;
+// import com.adobe.marketing.mobile.AndroidNetworkServiceOverrider.Connecting;
 
-import org.junit.After;
-import org.junit.Before;
+// import org.junit.After;
+// import org.junit.Before;
 
-// Intended to run all existing AndroidNetworkServiceTests with a replacement network handler.
-// (hence the extension of AndroidNetworkServiceTests).
-public class AndroidNetworkServiceOverriderTests extends AndroidNetworkServiceTests {
-	@Override
-	@Before
-	public void setup() {
-		AndroidNetworkServiceOverrider.setHTTPConnectionPerformer(new TestConnectionPerformer());
-		super.setup();
-	}
+// // Intended to run all existing AndroidNetworkServiceTests with a replacement network handler.
+// // (hence the extension of AndroidNetworkServiceTests).
+// public class AndroidNetworkServiceOverriderTests extends AndroidNetworkServiceTests {
+// 	@Override
+// 	@Before
+// 	public void setup() {
+// 		AndroidNetworkServiceOverrider.setHTTPConnectionPerformer(new TestConnectionPerformer());
+// 		super.setup();
+// 	}
 
-	@After
-	public void teardown() {
-		AndroidNetworkServiceOverrider.setHTTPConnectionPerformer(null);
-	}
+// 	@After
+// 	public void teardown() {
+// 		AndroidNetworkServiceOverrider.setHTTPConnectionPerformer(null);
+// 	}
 
-	// this is the custom network stack used for these tests
-	private static class TestConnectionPerformer extends HTTPConnectionPerformer {
-		private final String LOG_TAG = "TestConnectionPerformer";
+// 	// this is the custom network stack used for these tests
+// 	private static class TestConnectionPerformer extends HTTPConnectionPerformer {
+// 		private final String LOG_TAG = "TestConnectionPerformer";
 
-		TestConnectionPerformer() {
+// 		TestConnectionPerformer() {
 
-		}
+// 		}
 
-		@Override
-		public boolean shouldOverride(final String url, final  String method) {
-			Log.trace(LOG_TAG, "shouldOverride called for " + method + " request against " + url);
-			return true;
-		}
+// 		@Override
+// 		public boolean shouldOverride(final String url, final  String method) {
+// 			Log.trace(LOG_TAG, "shouldOverride called for " + method + " request against " + url);
+// 			return true;
+// 		}
 
-		@Override
-		public Connecting connect(final String url, final String method, final byte[] payload,
-								  final  Map<String, String> headers,
-								  final int connectionTimeoutSeconds, final int readTimeoutSeconds) {
-			Log.debug(LOG_TAG, "Overrode network connection to " + method + " for " + url);
+// 		@Override
+// 		public Connecting connect(final String url, final String method, final byte[] payload,
+// 								  final  Map<String, String> headers,
+// 								  final int connectionTimeoutSeconds, final int readTimeoutSeconds) {
+// 			Log.debug(LOG_TAG, "Overrode network connection to " + method + " for " + url);
 
-			try {
-				final URL dest = new URL(url);
-				final HttpURLConnection con = (HttpURLConnection) dest.openConnection();
-				con.setReadTimeout(readTimeoutSeconds * 1000);
-				con.setConnectTimeout(connectionTimeoutSeconds * 1000);
+// 			try {
+// 				final URL dest = new URL(url);
+// 				final HttpURLConnection con = (HttpURLConnection) dest.openConnection();
+// 				con.setReadTimeout(readTimeoutSeconds * 1000);
+// 				con.setConnectTimeout(connectionTimeoutSeconds * 1000);
 
-				con.setRequestMethod(method);
+// 				con.setRequestMethod(method);
 
-				if (headers != null) {
+// 				if (headers != null) {
 
-					for (final Map.Entry<String, String> entry : headers.entrySet()) {
-						con.setRequestProperty(entry.getKey(), entry.getValue());
-					}
+// 					for (final Map.Entry<String, String> entry : headers.entrySet()) {
+// 						con.setRequestProperty(entry.getKey(), entry.getValue());
+// 					}
 
-				}
+// 				}
 
-				if (method.equals("POST") && payload != null) {
-					con.setFixedLengthStreamingMode(payload.length);
-				}
+// 				if (method.equals("POST") && payload != null) {
+// 					con.setFixedLengthStreamingMode(payload.length);
+// 				}
 
-				con.connect();
+// 				con.connect();
 
-				if (method.equals("POST") && payload != null) {
-					final OutputStream os = new BufferedOutputStream(con.getOutputStream());
-					os.write(payload);
-					os.flush();
-					os.close();
-				}
+// 				if (method.equals("POST") && payload != null) {
+// 					final OutputStream os = new BufferedOutputStream(con.getOutputStream());
+// 					os.write(payload);
+// 					os.flush();
+// 					os.close();
+// 				}
 
-				final InputStream inputStream = con.getInputStream();
-				final InputStream errorStream = con.getErrorStream();
-				final int responseCode = con.getResponseCode();
-				final String responseMessage = con.getResponseMessage();
+// 				final InputStream inputStream = con.getInputStream();
+// 				final InputStream errorStream = con.getErrorStream();
+// 				final int responseCode = con.getResponseCode();
+// 				final String responseMessage = con.getResponseMessage();
 
-				return new Connecting() {
-					@Override
-					public InputStream getInputStream() {
-						Log.debug(LOG_TAG, "Input stream retrieved.");
+// 				return new Connecting() {
+// 					@Override
+// 					public InputStream getInputStream() {
+// 						Log.debug(LOG_TAG, "Input stream retrieved.");
 
-						return inputStream;
-					}
+// 						return inputStream;
+// 					}
 
-					@Override
-					public InputStream getErrorStream() {
-						return errorStream;
-					}
+// 					@Override
+// 					public InputStream getErrorStream() {
+// 						return errorStream;
+// 					}
 
-					@Override
-					public int getResponseCode() {
-						Log.debug(LOG_TAG, "Response code(%d) retrieved.", responseCode);
+// 					@Override
+// 					public int getResponseCode() {
+// 						Log.debug(LOG_TAG, "Response code(%d) retrieved.", responseCode);
 
-						return responseCode;
-					}
+// 						return responseCode;
+// 					}
 
-					@Override
-					public String getResponseMessage() {
-						Log.debug(LOG_TAG, "Response message(%s) retrieved. ", responseMessage);
+// 					@Override
+// 					public String getResponseMessage() {
+// 						Log.debug(LOG_TAG, "Response message(%s) retrieved. ", responseMessage);
 
-						return responseMessage;
-					}
+// 						return responseMessage;
+// 					}
 
-					@Override
-					public String getResponsePropertyValue(String responsePropertyKey) {
-						final String responseHeaderValue = con.getHeaderField(responsePropertyKey);
-						Log.debug(LOG_TAG, "Response header value (%s) retrieved for key (%s)", responseHeaderValue, responsePropertyKey);
-						return responseHeaderValue;
-					}
+// 					@Override
+// 					public String getResponsePropertyValue(String responsePropertyKey) {
+// 						final String responseHeaderValue = con.getHeaderField(responsePropertyKey);
+// 						Log.debug(LOG_TAG, "Response header value (%s) retrieved for key (%s)", responseHeaderValue, responsePropertyKey);
+// 						return responseHeaderValue;
+// 					}
 
-					@Override
-					public void close() {
-						Log.debug(LOG_TAG, "Connection closed.");
+// 					@Override
+// 					public void close() {
+// 						Log.debug(LOG_TAG, "Connection closed.");
 
-						if (inputStream != null) {
-							try {
-								inputStream.close();
-							} catch (final Exception ex) {
-								Log.error(LOG_TAG, "Error closing input stream(%s).", ex.getLocalizedMessage());
-							}
-						}
+// 						if (inputStream != null) {
+// 							try {
+// 								inputStream.close();
+// 							} catch (final Exception ex) {
+// 								Log.error(LOG_TAG, "Error closing input stream(%s).", ex.getLocalizedMessage());
+// 							}
+// 						}
 
-						con.disconnect();
-					}
-				};
+// 						con.disconnect();
+// 					}
+// 				};
 
 
-			} catch (final MalformedURLException ex) {
-				Log.error(LOG_TAG, "Invalid URL (%s)", ex.getLocalizedMessage());
-				return CONNECTION_ERROR_URL;
+// 			} catch (final MalformedURLException ex) {
+// 				Log.error(LOG_TAG, "Invalid URL (%s)", ex.getLocalizedMessage());
+// 				return CONNECTION_ERROR_URL;
 
-			} catch (final IOException ex) {
-				Log.error(LOG_TAG, "Unexpected exception in custom network override(%s)", ex.getLocalizedMessage());
-				return CONNECTION_ERROR_IO;
-			}
-		}
-	}
-}
+// 			} catch (final IOException ex) {
+// 				Log.error(LOG_TAG, "Unexpected exception in custom network override(%s)", ex.getLocalizedMessage());
+// 				return CONNECTION_ERROR_IO;
+// 			}
+// 		}
+// 	}
+// }

732bc71770f13c3e612a1317c5cef004ebf6819b
praveek <praveek@users.noreply.github.com>
2022-02-11 15:16:03 -0800

    Move rules engine from internal feature branch

---
+++ code/android-core-library/src/main/java/com/adobe/marketing/mobile/rulesengine/RulesEngine.java
@@ -0,0 +1,48 @@
+/*
+  Copyright 2022 Adobe. All rights reserved.
+  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under
+  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+  OF ANY KIND, either express or implied. See the License for the specific language
+  governing permissions and limitations under the License.
+ */
+
+package com.adobe.marketing.mobile.rulesengine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RulesEngine<T extends Rule> {
+	Evaluating evaluator;
+	Transforming transformer;
+	List<T> rules;
+
+	public RulesEngine(final Evaluating evaluator, final Transforming transformer) {
+		this.evaluator = evaluator;
+		this.transformer = transformer;
+	}
+
+	public List<Rule> evaluate(final TokenFinder tokenFinder) {
+		final Context context = new Context(tokenFinder, evaluator, transformer);
+		List<Rule> triggerRules = new ArrayList<>();
+
+		for (final Rule rule : rules) {
+			if (rule.getEvaluable().evaluate(context).isSuccess()) {
+				triggerRules.add(rule);
+			}
+		}
+
+		return triggerRules;
+	}
+
+	public void addRules(final List<T> newRules) {
+		rules.addAll(newRules);
+	}
+
+	public void clearRules() {
+		rules.clear();
+	}
+
+}

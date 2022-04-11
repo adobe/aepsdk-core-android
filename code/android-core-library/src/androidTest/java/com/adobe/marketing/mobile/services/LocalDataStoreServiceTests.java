b03ba78a81b5d453788666c43b65e28879696cfe
Prashanth Rudrabhat <prudrabhat@adobe.com>
2022-04-11 12:28:16 -0700

    [#39] Migrate aepsdk-core-android to AndroidX
    [Summary]
    - Uses the AndroidX migration tool for preliminary migration.
    - Manually grep for reflection use inside the project for class mapping.
      Following instance needed a  manual change :
        * android.support.v4.app.NotificationCompat -> androidx.core.app.NotificationCompat
    - Following instances of instrumentation tests needed change manually :
        * InstrumentationRegistry.getContext() -> InstrumentationRegistry.getInstrumentation().getContext()
    - androidx.test.ext:junit:1.1.3 was added for AndroidJUnit4 test support.
    
    [Testing]
    - Used the TestApp to trigger changes and ensured that change surface works.
    - Instrumentation and unit tests pass

--- code/android-core-library/src/androidTest/java/com/adobe/marketing/mobile/services/LocalDataStoreServiceTests.java
+++ code/android-core-library/src/androidTest/java/com/adobe/marketing/mobile/services/LocalDataStoreServiceTests.java
@@ -17,8 +17,8 @@ import static junit.framework.Assert.assertNull;
 import static junit.framework.Assert.assertTrue;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
--- code/testapp/src/main/java/com/adobe/testapp/PlatformServicesFragment.java
+++ code/testapp/src/main/java/com/adobe/testapp/PlatformServicesFragment.java
@@ -12,7 +12,7 @@
 package com.adobe.testapp;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

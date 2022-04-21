91b4d3443e6ba51b7e83d5da303889a65d48e338
praveek <praveek@users.noreply.github.com>
2022-04-21 14:47:03 -0700

    Run code format and add headers

--- code/android-core-library/src/main/java/com/adobe/marketing/mobile/internal/eventhub/EventHub.kt
+++ code/android-core-library/src/main/java/com/adobe/marketing/mobile/internal/eventhub/EventHub.kt
@@ -1,21 +1,37 @@
+/*
+  Copyright 2022 Adobe. All rights reserved.
+  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under
+  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+  OF ANY KIND, either express or implied. See the License for the specific language
+  governing permissions and limitations under the License.
+ */
+
 package com.adobe.marketing.mobile.internal.eventhub
 
-import com.adobe.marketing.mobile.*
-import java.util.concurrent.*
+import com.adobe.marketing.mobile.Extension
+import com.adobe.marketing.mobile.LoggingMode
+import com.adobe.marketing.mobile.MobileCore
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 /**
  * EventHub class is responsible for delivering events to listeners and maintaining registered extension's lifecycle.
  */
 internal class EventHub {
+    constructor()
+
     companion object {
-        val LOG_TAG  = "EventHub"
+        val LOG_TAG = "EventHub"
         public var shared = EventHub()
     }
 
     private val eventHubExecutor: ExecutorService by lazy { Executors.newSingleThreadExecutor() }
     private val registeredExtensions: ConcurrentHashMap<String, ExtensionContainer> = ConcurrentHashMap()
-    private var hubStarted = false;
-
+    private var hubStarted = false
 
     init {
         registerExtension(EventHubPlaceholderExtension::class.java) {}
@@ -27,7 +43,7 @@ internal class EventHub {
     fun start() {
         eventHubExecutor.submit {
             this.hubStarted = true
-            
+
             this.shareEventHubSharedState()
             MobileCore.log(LoggingMode.DEBUG, LOG_TAG, "Event Hub successfully started")
         }
@@ -100,6 +116,6 @@ internal class EventHub {
     }
 }
 
-/// Helper to get extension type name
+// / Helper to get extension type name
 private val Class<out Extension>.extensionTypeName
     get() = this.name

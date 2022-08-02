31ee9948fa5d76bd8d678d08f0eab68372625bfa
yansong <yanyang@adobe.com>
2022-08-02 16:23:10 -0600

    format

--- code/android-core-library/src/test/java/com/adobe/marketing/mobile/launch/rulesengine/LaunchTokenFinderTest.kt
+++ code/android-core-library/src/test/java/com/adobe/marketing/mobile/launch/rulesengine/LaunchTokenFinderTest.kt
@@ -22,7 +22,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito

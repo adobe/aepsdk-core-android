b6896a789570d4330359352dd8aac64367caa168
praveek <praveek@users.noreply.github.com>
2023-01-30 15:29:22 -0800

    Fix docs for deprecated methods

--- code/identity/src/phone/java/com/adobe/marketing/mobile/Identity.java
+++ code/identity/src/phone/java/com/adobe/marketing/mobile/Identity.java
@@ -45,12 +45,9 @@ public class Identity {
     }
 
     /**
-     * Registers the Identity extension with the {@code MobileCore}
-     *
-     * <p>
-     *
-     * <p>This will allow the extension to send and receive events to and from the SDK.
-     */
+	 * Registers the extension with the Mobile SDK. This method should be called only once in your application class.
+	 * @deprecated as of 2.0.0, use {@link MobileCore#registerExtensions(List, AdobeCallback)} with {@link Identity#EXTENSION} instead.
+	 */
     @Deprecated
     public static void registerExtension() {
         MobileCore.registerExtension(

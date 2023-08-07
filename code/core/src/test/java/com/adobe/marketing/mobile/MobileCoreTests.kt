59f7e719556bb449f155a26c3fa5a919f2495e72
praveek <praveek@users.noreply.github.com>
2023-08-07 23:47:08 +0000

    Update versions [Core-2.3.1]

--- code/core/src/test/java/com/adobe/marketing/mobile/MobileCoreTests.kt
+++ code/core/src/test/java/com/adobe/marketing/mobile/MobileCoreTests.kt
@@ -35,7 +35,7 @@ import kotlin.test.assertTrue
 @RunWith(MockitoJUnitRunner.Silent::class)
 class MobileCoreTests {
 
-    private var EXTENSION_VERSION = "2.3.0"
+    private var EXTENSION_VERSION = "2.3.1"
 
     @Mock
     private lateinit var mockedEventHub: EventHub

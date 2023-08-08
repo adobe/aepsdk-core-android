fbdbf416fbb14399a9725760d01cd800e2b5f598
Ryan Morales <rymorale@adobe.com>
2023-08-08 09:13:58 -0700

    bump version to 2.3.1

--- code/core/src/main/java/com/adobe/marketing/mobile/internal/CoreConstants.kt
+++ code/core/src/main/java/com/adobe/marketing/mobile/internal/CoreConstants.kt
@@ -13,7 +13,7 @@ package com.adobe.marketing.mobile.internal
 
 internal object CoreConstants {
     const val LOG_TAG = "MobileCore"
-    const val VERSION = "2.3.0"
+    const val VERSION = "2.3.1"
 
     object EventDataKeys {
         /**

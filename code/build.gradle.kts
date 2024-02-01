724e446dabf9936c58dc38f27630a9f7fefbf5e3
praveek <praveek@users.noreply.github.com>
2024-02-01 15:23:54 -0800

    Update Android CircleCI executor for unit tests

--- code/build.gradle.kts
+++ code/build.gradle.kts
@@ -9,6 +9,7 @@ buildscript {
 
 
     dependencies {
+        // Todo: Move away from specifying commit once we release a stable version."
         classpath("com.github.adobe:aepsdk-commons:9deb721db7")
         classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.13.2")
         classpath("androidx.benchmark:benchmark-gradle-plugin:1.2.3")

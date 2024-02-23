08488fc19a92dfa2adfa0aee6da5fdb4b1773600
yansong <yanyang@adobe.com>
2024-02-23 15:36:21 -0600

    format

--- code/core/src/androidTest/java/com/adobe/marketing/mobile/internal/util/NetworkUtilsTest.kt
+++ code/core/src/androidTest/java/com/adobe/marketing/mobile/internal/util/NetworkUtilsTest.kt
@@ -8,6 +8,7 @@
   OF ANY KIND, either express or implied. See the License for the specific language
   governing permissions and limitations under the License.
 */
+
 package com.adobe.marketing.mobile.internal.util
 
 import android.net.ConnectivityManager

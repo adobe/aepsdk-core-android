86eeaa1513109e9b3008650ec84d681806d42327
yansong <yanyang@adobe.com>
2024-05-10 15:14:26 -0500

    Update Core to 2.6.3

--- code/core/src/test/java/com/adobe/marketing/mobile/internal/configuration/ConfigurationExtensionTests.kt
+++ code/core/src/test/java/com/adobe/marketing/mobile/internal/configuration/ConfigurationExtensionTests.kt
@@ -60,7 +60,7 @@ import kotlin.test.assertTrue
 @RunWith(MockitoJUnitRunner.Silent::class)
 class ConfigurationExtensionTests {
 
-    private var EXTENSION_VERSION = "2.6.2"
+    private var EXTENSION_VERSION = "2.6.3"
 
     @Mock
     private lateinit var mockServiceProvider: ServiceProvider

b0d9f0c7594765906a7853bdb0c47f164f6ebb5d
PratthamArora <arora.prattham@gmail.com>
2024-05-15 10:04:05 +0530

    MOB-20075: refactor unit tests for PushTemplateType

--- code/core/src/test/java/com/adobe/marketing/mobile/services/ui/notification/PushTemplateTypeTest.kt
+++ code/core/src/test/java/com/adobe/marketing/mobile/services/ui/notification/PushTemplateTypeTest.kt
@@ -17,46 +17,14 @@ import kotlin.test.assertEquals
 class PushTemplateTypeTest {
 
     @Test
-    fun `test fromString returns BASIC value`() {
-        val templateType = PushTemplateType.fromString("basic")
-        assertEquals(templateType, PushTemplateType.BASIC)
+    fun `test for fromString for each of the enum values`() {
+        PushTemplateType.values().forEach { value ->
+            assertEquals(value, PushTemplateType.fromString(value.value))
+        }
     }
 
     @Test
-    fun `test fromString returns CAROUSEL value`() {
-        val templateType = PushTemplateType.fromString("car")
-        assertEquals(templateType, PushTemplateType.CAROUSEL)
-    }
-
-    @Test
-    fun `test fromString returns INPUT_BOX value`() {
-        val templateType = PushTemplateType.fromString("input")
-        assertEquals(templateType, PushTemplateType.INPUT_BOX)
-    }
-
-    @Test
-    fun `test fromString returns UNKNOWN value`() {
-        val templateType = PushTemplateType.fromString("templateType")
-        assertEquals(templateType, PushTemplateType.UNKNOWN)
-    }
-
-    @Test
-    fun `test getValue returns basic value for BASIC`() {
-        assertEquals(PushTemplateType.BASIC.value, "basic")
-    }
-
-    @Test
-    fun `test getValue returns car value for CAROUSEL`() {
-        assertEquals(PushTemplateType.CAROUSEL.value, "car")
-    }
-
-    @Test
-    fun `test getValue returns input value for INPUT_BOX`() {
-        assertEquals(PushTemplateType.INPUT_BOX.value, "input")
-    }
-
-    @Test
-    fun `test getValue returns unknown value for UNKNOWN`() {
-        assertEquals(PushTemplateType.UNKNOWN.value, "unknown")
+    fun `test to ensure that invalid values map to unknown value`() {
+        assertEquals(PushTemplateType.UNKNOWN, PushTemplateType.fromString("SomeRandomValue"))
     }
 }

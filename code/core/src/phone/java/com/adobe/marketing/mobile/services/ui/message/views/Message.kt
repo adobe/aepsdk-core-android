7aa27cfceabdbab95f5be9bf98264f75019fface
Navratan Soni <navratans@adobe.com>
2024-07-30 14:56:03 +0530

    Spotless apply

--- code/core/src/phone/java/com/adobe/marketing/mobile/services/ui/message/views/Message.kt
+++ code/core/src/phone/java/com/adobe/marketing/mobile/services/ui/message/views/Message.kt
@@ -11,12 +11,10 @@
 
 package com.adobe.marketing.mobile.services.ui.message.views
 
-import android.view.Window
 import android.webkit.WebView
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalView

d01619ae6a566dde77f3fcf0b0e1316193fe6651
Ishita Gambhir <igambhir@adobe.com>
2024-09-18 16:35:09 +0530

    add new error codes for AdobeError

--- code/core/src/main/java/com/adobe/marketing/mobile/AdobeError.java
+++ code/core/src/main/java/com/adobe/marketing/mobile/AdobeError.java
@@ -27,6 +27,15 @@ public class AdobeError implements Serializable {
     /** when a callback is null. */
     public static final AdobeError CALLBACK_NULL = new AdobeError("general.callback.null", 2);
 
+    /** when a server error happens. */
+    public static final AdobeError SERVER_ERROR = new AdobeError("general.callback.server.error", 4);
+
+    /** when a network error happens. */
+    public static final AdobeError NETWORK_ERROR = new AdobeError("general.callback.network.error", 5);
+
+    /** when an invalid request is made. */
+    public static final AdobeError INVALID_REQUEST = new AdobeError("general.callback.invalid.request", 6);
+
     /** when a extension is not initialized. */
     public static final AdobeError EXTENSION_NOT_INITIALIZED =
             new AdobeError("general.extension.not.initialized", 11);

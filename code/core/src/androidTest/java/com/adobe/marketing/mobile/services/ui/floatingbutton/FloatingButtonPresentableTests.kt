47bb4e7710d5c092938a7b408fea95544e8fc396
Prashanth Rudrabhat <prudrabhat@adobe.com>
2025-02-04 13:15:09 -0800

    Formatting fixes

--- code/core/src/androidTest/java/com/adobe/marketing/mobile/services/ui/floatingbutton/FloatingButtonPresentableTests.kt
+++ code/core/src/androidTest/java/com/adobe/marketing/mobile/services/ui/floatingbutton/FloatingButtonPresentableTests.kt
@@ -1,3 +1,14 @@
+/*
+  Copyright 2025 Adobe. All rights reserved.
+  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under
+  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+  OF ANY KIND, either express or implied. See the License for the specific language
+  governing permissions and limitations under the License.
+*/
+
 import android.content.Context
 import android.view.ContextThemeWrapper
 import androidx.test.core.app.ApplicationProvider
@@ -16,8 +27,8 @@ import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 
 class FloatingButtonPresentableTests {
@@ -64,7 +75,9 @@ class FloatingButtonPresentableTests {
         val theme = themedContext.theme
         // get android.background from the theme
         val background = theme.obtainStyledAttributes(intArrayOf(android.R.attr.background))
-        assertTrue(background.hasValue(0)
-                && background.peekValue(0).resourceId == android.R.color.transparent)
+        assertTrue(
+            background.hasValue(0) &&
+                background.peekValue(0).resourceId == android.R.color.transparent
+        )
     }
 }
